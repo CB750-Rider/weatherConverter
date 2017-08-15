feedfacb05a12455fd646cb71fec4726a8322417
Dr. Lee Burchett <lee.r.burchett@gmail.com>
2017-08-15 07:19:30 -0400

    Update after copying from laptop.

--- weatherConversion.h
+++ weatherConversion.h
@@ -1,10 +1,36 @@
 /*
- * weatherConversion.h
+ * weatherConversion.c
  *
  *  Created on: Aug 11, 2016
- *      Author: Dr. Lee Burchett 
+ *      Author: Dr. Lee Burchett  
  *
- *      C code for converting/populating weather data.
+ *  Last Modified: 6 June, 2017
+ * 
+ * Thes file contains header prototypes and tools for routines that are useful 
+ * for converting the myriad ways that researchers use to represent humidity in 
+ * the atmosphere. The goal is to provide a tool to save time on writing and 
+ * debugging these conversions. If any conversion data appear suspect. Please 
+ * contact me with suggested changes at lee.r.burchett@gmail.com.
+ *
+ * Copyright (c) 2016 Dr. Lee Burchett
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
  */
 
 #ifndef WEATHERCONVERSION_H_
